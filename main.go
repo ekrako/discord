5d5b7dd5625fa6190860678fcf7a7655c15d161c
Eran Krakovsky <ekrako@gmail.com>
2017-12-27 00:19:41 +0200

    Finished General Porpose Timer with Rest API

--- main.go
+++ main.go
@@ -1,34 +1,186 @@
 package main
 
 import (
+	"context"
 	"log"
-	"time"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 
 	"github.com/ekrako/discord/timer"
 )
 
 func main() {
-	timer.Health()
-	t := timer.Create("ID", "https://requestb.in/14tpopr1", "Hello World 1", true, 1)
-	t = timer.Create("ID2", "https://requestb.in/14tpopr1", "Hello World 2", true, 1)
-	t.SetInterval(500 * time.Millisecond)
-	log.Println("Change 0.5  time.Second")
-	t.Run()
-	time.Sleep(5 * time.Second)
-	t.Message = "Hi There 2"
-	log.Println("Changed message to timer 2 to hi there")
-	t = timer.Get("ID3")
-	t.Run()
-	time.Sleep(5 * time.Second)
-	t.SetInterval(time.Second)
-	log.Println("Change interval Second")
-	time.Sleep(5 * time.Second)
-	log.Println("stopping ticker...")
-	t.Stop()
-	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root."))
+	})
+
+	// RESTy routes for "articles" resource
+	r.Route("/timers", func(r chi.Router) {
+		r.Get("/", ListTimers)
+		r.Post("/", CreateTimer)
+		r.Route("/{timerID}", func(r chi.Router) {
+			r.Use(TimerCtx)
+			r.Get("/", GetTimer)
+			r.Put("/", UpdateTimer)
+			r.Delete("/", DeleteTimer)
+			r.Post("/start", StartTimer)
+			r.Delete("/start", StopTimer)
+			r.Post("/stop", StopTimer)
+			r.Delete("/stop", StartTimer)
+		})
+
+	})
+	log.Fatal(http.ListenAndServe(":3333", r))
 }
 
-// ID:          "s.ID",
-// Destination: "https://requestb.in/14tpopr1",
-// Message:     "s.Message",
-// Enabled:     true,
+// ListTimers List all the timers in server
+func ListTimers(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, timer.GetAllTimers())
+}
+
+func TimerCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var thisTimer *timer.SingleTimer
+		var err error
+
+		if timerID := chi.URLParam(r, "timerID"); timerID != "" {
+			thisTimer, err = timer.Get(timerID)
+		} else {
+			render.Render(w, r, ErrNotFound)
+			return
+		}
+		if err != nil {
+			render.Render(w, r, ErrNotFound)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "timer", thisTimer)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// CreateTimer Creates a new time
+func CreateTimer(w http.ResponseWriter, r *http.Request) {
+	data := &timer.Request{}
+	if err := render.Bind(r, data); err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+	t, err := timer.Create(*data)
+	if err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+	render.Status(r, http.StatusCreated)
+	render.JSON(w, r, t)
+}
+
+// GetTimer returns the specific Timer.
+func GetTimer(w http.ResponseWriter, r *http.Request) {
+	thisTimer := r.Context().Value("timer").(*timer.SingleTimer)
+	render.JSON(w, r, thisTimer)
+}
+
+//StartTimer Make the timer running
+func StartTimer(w http.ResponseWriter, r *http.Request) {
+	thisTimer := r.Context().Value("timer").(*timer.SingleTimer)
+	thisTimer.Start()
+	render.JSON(w, r, thisTimer)
+}
+
+//StopTimer Stop the timer running
+func StopTimer(w http.ResponseWriter, r *http.Request) {
+	thisTimer := r.Context().Value("timer").(*timer.SingleTimer)
+	thisTimer.Stop()
+	render.JSON(w, r, thisTimer)
+}
+
+// UpdateTimer the parameters of a specific Time specific Timer.
+func UpdateTimer(w http.ResponseWriter, r *http.Request) {
+	data := &timer.Request{}
+	if err := render.Bind(r, data); err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+	thisTimer := r.Context().Value("timer").(*timer.SingleTimer)
+	thisTimer.Update(*data)
+	render.JSON(w, r, thisTimer)
+}
+
+// Delete an existing timer
+func DeleteTimer(w http.ResponseWriter, r *http.Request) {
+	thisTimer := r.Context().Value("timer").(*timer.SingleTimer)
+
+	thisTimer.Delete()
+
+	render.NoContent(w, r)
+}
+
+// Binder interface for managing request payloads.
+type Binder interface {
+	Bind(r *http.Request) error
+}
+
+// Bind decodes a request body and executes the Binder method of the
+// payload structure.
+func Bind(r *http.Request, v Binder) error {
+	if err := render.Decode(r, v); err != nil {
+		return err
+	}
+	return render.Bind(r, v)
+}
+
+//--
+// Error response payloads & renderers
+//--
+
+// ErrResponse renderer type for handling all sorts of errors.
+//
+// In the best case scenario, the excellent github.com/pkg/errors package
+// helps reveal information on the error, setting it on Err, and in the Render()
+// method, using it to set the application-specific error code in AppCode.
+type ErrResponse struct {
+	Err            error `json:"-"` // low-level runtime error
+	HTTPStatusCode int   `json:"-"` // http response status code
+
+	StatusText string `json:"status"`          // user-level status message
+	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func ErrInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
+}
+
+func ErrRender(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 422,
+		StatusText:     "Error rendering response.",
+		ErrorText:      err.Error(),
+	}
+}
+
+var ErrNotFound = &ErrResponse{HTTPStatusCode: 404, StatusText: "Resource not found."}
--- timer/timer.go
+++ timer/timer.go
@@ -1,6 +1,7 @@
 package timer
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -8,29 +9,45 @@ import (
 	"log"
 )
 
-type TimerSettings struct {
-	ID          string `json:"_id"`
+//Request JSON represent the timer JSON reuests
+type Request struct {
+	ID          string `json:"id"`
 	Destination string `json:"destination"`
 	Interval    uint   `json:"interval"`
 	Message     string `json:"message"`
 	Enabled     bool   `json:"enabled"`
 }
 
+//Bind the data to json request
+func (s *Request) Bind(r *http.Request) error {
+	return nil
+}
+
+//SingleTimer is the timer Object
 type SingleTimer struct {
 	ID          string
 	Destination string
 	Message     string
+	Running     bool
+	Timing      uint `json:"interval"`
 	interval    time.Duration
 	Enabled     bool
 	ticker      *time.Ticker
 	quit        chan struct{}
-	deleted     bool
 }
 
-var timers map[string]SingleTimer
+var timers map[string]*SingleTimer
+var userTimers map[string][]*SingleTimer
+
+const resolution time.Duration = time.Second
 
 func init() {
-	timers = make(map[string]SingleTimer)
+	timers = make(map[string]*SingleTimer)
+}
+
+//GetAllTimers Get All timers
+func GetAllTimers() *map[string]*SingleTimer {
+	return &timers
 }
 
 //Health Check
@@ -39,59 +56,81 @@ func Health() {
 }
 
 //Create a new timer
-func Create(id string, destination string, messege string, enabled bool, interval uint) *SingleTimer {
-	// func Create() *SingleTimer {
+func Create(s Request) (*SingleTimer, error) {
+	if timers[s.ID] != nil {
+		return nil, fmt.Errorf("timer id %s already exists", s.ID)
+	}
 	t := SingleTimer{
-
-		ID:          id,
-		Destination: destination,
-		Message:     messege,
-		Enabled:     enabled,
+		ID:          s.ID,
+		Destination: s.Destination,
+		Message:     s.Message,
+		Enabled:     s.Enabled,
 	}
 	log.Println("new timer", t.Message)
-	t.interval = time.Duration(interval) * time.Second
+	t.interval = time.Duration(s.Interval) * resolution
+	t.Timing = s.Interval
 	t.quit = make(chan struct{})
-	timers[id] = t
-	return &t
+	timers[s.ID] = &t
+	return &t, nil
 }
 
 //Get - get timer by ID
-func Get(id string) *SingleTimer {
+func Get(id string) (*SingleTimer, error) {
 	t := timers[id]
-	return &t
+	if t == nil {
+		return nil, fmt.Errorf("timer id %q does not exists", id)
+	}
+	return t, nil
+}
+
+// Update the timer parameters
+func (s *SingleTimer) Update(r Request) {
+	s.Destination = r.Destination
+	s.Message = r.Message
+	s.Enabled = r.Enabled
+	s.SetInterval(time.Duration(r.Interval) * resolution)
 }
 
 //Stop the Timer
 func (s *SingleTimer) Stop() {
-	if s.deleted {
+	if !s.Running {
 		return
 	}
-	s.deleted = true
+	s.Running = false
 
-	close(s.quit)
+	s.quit <- struct{}{}
+}
+
+//Delete the timer
+func (s *SingleTimer) Delete() {
+	delete(timers, s.ID)
 }
 
 //SetInterval - set the timer interval
 func (s *SingleTimer) SetInterval(interval time.Duration) {
 	s.interval = interval
+	s.Timing = uint(interval / resolution)
 	if s.ticker != nil {
-		s.ticker.Stop()
-		s.ticker = time.NewTicker(s.interval)
+		s.Stop()
+		s.Start()
 	}
 }
 
-//Run - Make the timer work
-func (s *SingleTimer) Run() {
-
+//Start - Make the timer work
+func (s *SingleTimer) Start() {
+	if s.Running {
+		return
+	}
+	s.Running = true
 	go func() {
 		s.ticker = time.NewTicker(s.interval)
+
 		defer s.ticker.Stop()
 		for {
 			select {
 			case <-s.ticker.C:
 				if s.Enabled {
 					log.Println(s.Message)
-					log.Println(s.interval)
 
 					resp, err := http.Post(s.Destination, "application/json", strings.NewReader(s.Message))
 					if err != nil {
